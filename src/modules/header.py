860074540d8375ed33bd7439a359a86b824b1a1a
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-10 14:54:15 +0700

    Update header.py

--- src/modules/header.py
+++ src/modules/header.py
@@ -11,12 +11,15 @@ from modules.include.post_mapping_and_alignment.markduplicates import markduplic
 from modules.include.post_mapping_and_alignment.baserecalibrator_and_applyBQSR import baserecalibrator, applyBQSR
 from modules.include.variant_calling.genomic_SNPs_and_Indels_calling_GATK import genomic_SNPs_and_Indels_calling_GATK
 from modules.include.post_variant_calling.genomic_variant_combination import combine_gvcfs
+from modules.include.post_variant_calling.variant_genotyping import genotype_gvcfs
 from modules.include.post_variant_calling.variant_normalization import variant_normalization
 from modules.include.post_variant_calling.variant_filtration import hard_filtration
+from include.annotaion.SnpEff_and_SnpSift_annotation import snpEff_and_snpSift_annotation
 from modules.flow.mapping_and_alignment_flow import mapping_and_alignment_flow
 from modules.flow.post_mapping_and_alignment_flow import post_mapping_and_alignment_flow
 from modules.flow.variant_calling_flow import variant_calling_flow
 from modules.flow.post_variant_calling_flow import post_variant_calling_flow
+from modules.flow.variant_annotation_flow import variant_annotation_flow
 # Set Java options for memory management
 env = os.environ.copy()
 env["JAVA_OPTS"] = "-Xmx8g"
