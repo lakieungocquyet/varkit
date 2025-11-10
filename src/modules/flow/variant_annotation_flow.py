74e01ed76e42242191f3e6d034b8fde745d10a49
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-10 22:15:56 +0700

    Update variant_annotation_flow.py

--- src/modules/flow/variant_annotation_flow.py
+++ src/modules/flow/variant_annotation_flow.py
@@ -4,5 +4,5 @@ def variant_annotation_flow(GLOBAL_GVCF_LIST, OUTDIR):
     snpEff_and_snpSift_annotation(
         NORMALIZED_GVCF_FILE = GLOBAL_GVCF_LIST["normalized_gvcf_file"], 
         OUTDIR = OUTDIR, 
-        ANNOTATED_GVCF_FILE = GLOBAL_GVCF_LIST["cohort.annotated.g.vcf"]
+        ANNOTATED_GVCF_FILE = GLOBAL_GVCF_LIST["snpEff_and_snpSift_annotated_gvcf_file"]
         )
