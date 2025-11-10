4f7d5ded796f47cfca975176808517cc9cd15d9e
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-10 11:32:01 +0700

    Create mapping_and_alignment_flow.py

---
+++ src/modules/flow/mapping_and_alignment_flow.py
@@ -0,0 +1,32 @@
+from modules.header import *
+
+def mapping_and_alignment_flow(SAMPLE_LIST, REFERENCE_LIST, OUTDIR):
+    for sample_id, info in SAMPLE_LIST.items():
+        start_time = time.time()
+        logging.info(f"Mapping and alignment sample: {sample_id}")
+        if info["read_length_type"] == "short":
+            mapping_and_alignment_BWA_mem(
+                SAMPLE_ID=sample_id,
+                PLATFORM=info["platform"],
+                FORWARD=info["read1"],
+                REVERSE=info["read2"],
+                REFERENCE=REFERENCE_LIST["genome"],
+                OUTDIR=OUTDIR,
+                SAMPLE_OUTDIR=info["sample_outdir"],
+                SAM_FILE=info["sam_file"]
+            )
+        else:
+            mapping_and_alignment_Minimap2(
+                SAMPLE_ID=sample_id,
+                PLATFORM=info["platform"],
+                FORWARD=info["read1"],
+                REVERSE=info["read2"],
+                REFERENCE=REFERENCE_LIST["genome"],
+                OUTDIR=OUTDIR,
+                SAMPLE_OUTDIR=info["sample_outdir"],
+                SAM_FILE=info["sam_file"]
+            )
+        end_time = time.time()
+        duration = (end_time - start_time) / 60  
+        logging.info(f"{sample_id} finished mapping and alignment in {duration:.2f} minutes")
+    logging.info("All samples finished mapping and alignment step.")
