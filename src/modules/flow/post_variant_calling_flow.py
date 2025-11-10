1a48ac60093d26c785e9533239804ff3d2a7c58d
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-10 14:54:09 +0700

    Update post_variant_calling_flow.py

--- src/modules/flow/post_variant_calling_flow.py
+++ src/modules/flow/post_variant_calling_flow.py
@@ -11,10 +11,20 @@ def post_variant_calling_flow(SAMPLE_LIST, REFERENCE_LIST, OUTDIR, GLOBAL_GVCF_L
         OUTDIR = OUTDIR,
         COMBINED_GVCF_FILE = GLOBAL_GVCF_LIST["combined_gvcf_file"]
     )
-    
-    hard_filtration(
+    genotype_gvcfs(
         COMBINED_GVCF_FILE = GLOBAL_GVCF_LIST["combined_gvcf_file"], 
+        REFERENCE = REFERENCE_LIST["genome"], 
+        OUTDIR = OUTDIR, 
+        COHORT_GVCF_FILE = GLOBAL_GVCF_LIST["cohort_gvcf_file"]
+        )
+    hard_filtration(
+        COHORT_GVCF_FILE = GLOBAL_GVCF_LIST["cohort_gvcf_file"], 
         REFERENCE = REFERENCE_LIST["genome"],
         OUTDIR = OUTDIR, 
         FILTERED_GVCF_FILE = GLOBAL_GVCF_LIST["filtered_gvcf_file"]
     )
+    variant_normalization(
+        FILTERED_GVCF_FILE = GLOBAL_GVCF_LIST["filtered_gvcf_file"],  
+        OUTDIR = OUTDIR, 
+        NORMALIZED_GVCF_FILE = GLOBAL_GVCF_LIST["normalized_gvcf_file"]
+        )
