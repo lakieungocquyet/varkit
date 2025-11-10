c33f55f94c9163e7ce0c90106b93c7818365bba5
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-10 22:15:46 +0700

    Update post_variant_calling_flow.py

--- src/modules/flow/post_variant_calling_flow.py
+++ src/modules/flow/post_variant_calling_flow.py
@@ -1,12 +1,9 @@
 from modules.header import *
 
-def post_variant_calling_flow(SAMPLE_LIST, REFERENCE_LIST, OUTDIR, GLOBAL_GVCF_LIST):
-    GVCF_FILE_LIST = ""
-    for sample_id, info in SAMPLE_LIST.items():
-        GVCF_FILE_LIST += f" -V {info['sample_outdir']}/{info['raw_gvcf_file']}"    
+def post_variant_calling_flow(GVCF_FILE_STRING, REFERENCE_LIST, OUTDIR, GLOBAL_GVCF_LIST):
 
     combine_gvcfs(
-        GVCF_FILE_LIST = GVCF_FILE_LIST,
+        GVCF_FILE_STRING = GVCF_FILE_STRING,
         REFERENCE = REFERENCE_LIST["genome"],
         OUTDIR = OUTDIR,
         COMBINED_GVCF_FILE = GLOBAL_GVCF_LIST["combined_gvcf_file"]
