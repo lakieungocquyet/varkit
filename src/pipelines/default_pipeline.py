0db70537ede101e81233c1bedf75d0c4a8a4423d
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-10 22:16:18 +0700

    Update default_pipeline.py

--- src/pipelines/default_pipeline.py
+++ src/pipelines/default_pipeline.py
@@ -1,50 +1,44 @@
 from modules.header import *
 
 def default_pipeline(INPUT_YAML):
-
-    SAMPLES, GENOME, KNOWN_SITES, OUTDIR = initialize_from_yaml(
-        INPUT_YAML = INPUT_YAML
-        )
-    known_sites_string = ""
-    if KNOWN_SITES:
-        for site in KNOWN_SITES:
-            known_sites_string += f"--known-sites {site} "
-    SAMPLE_LIST, REFERENCE_LIST, GLOBAL_GVCF_LIST = init_samples(
-        SAMPLES = SAMPLES, 
-        GENOME = GENOME, 
-        KNOWN_SITES = KNOWN_SITES, 
-        OUTDIR = OUTDIR
-        )
-    
+    # [STAGE 1]: UPSTREAM PROCESSING
+    OUTDIR, SAMPLE_LIST, REFERENCE_LIST, GLOBAL_GVCF_LIST, GVCF_FILE_STRING, KNOWN_SITES_STRING = upstream_processing_flow(INPUT_YAML = INPUT_YAML)
     setup_logger(outdir = OUTDIR)
-    # [STAGE 1]: MAPPING AND ALIGNMENT
+    # [STAGE 2]: MAPPING AND ALIGNMENT
     mapping_and_alignment_flow(
         SAMPLE_LIST = SAMPLE_LIST, 
         REFERENCE_LIST = REFERENCE_LIST,
         OUTDIR = OUTDIR
         )
-    # [STAGE 2]: POST MAPPING AND ALIGNMENT
+    # [STAGE 3]: POST MAPPING AND ALIGNMENT
     post_mapping_and_alignment_flow(
         SAMPLE_LIST = SAMPLE_LIST, 
-        known_sites_string = known_sites_string, 
+        KNOWN_SITES_STRING = KNOWN_SITES_STRING, 
         OUTDIR = OUTDIR
         )
-    # [STAGE 3]: VARIANT CALLING
+    # [STAGE 4]: VARIANT CALLING
     variant_calling_flow(
         SAMPLE_LIST = SAMPLE_LIST, 
         OUTDIR = OUTDIR
         )
-    # [STAGE 4]: POST VARIANT CALLING
+    # [STAGE 5]: POST VARIANT CALLING
     post_variant_calling_flow(
+        GVCF_FILE_LIST = GVCF_FILE_STRING,
         SAMPLE_LIST = SAMPLE_LIST, 
         REFERENCE_LIST = REFERENCE_LIST, 
         OUTDIR = OUTDIR,
         GLOBAL_GVCF_LIST = GLOBAL_GVCF_LIST
         )
-    # [STAGE 5]: VARIANT ANNOTATION
+    # [STAGE 6]: VARIANT ANNOTATION
     variant_annotation_flow(
         GLOBAL_GVCF_LIST = GLOBAL_GVCF_LIST, 
         OUTDIR = OUTDIR, 
         )
-
+    # [STAGE 7]: DOWNSTREAM PROCESSING
+    downstream_processing_flow(
+        SAMPLE_LIST = SAMPLE_LIST, 
+        GLOBAL_GVCF_LIST = GLOBAL_GVCF_LIST, 
+        REFERENCE_LIST = REFERENCE_LIST, 
+        OUTDIR = OUTDIR
+    )
     return 0
