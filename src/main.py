9320ff6aa617164dbbdc50cebccceec8ff78c18b
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-10 11:31:57 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,18 +8,12 @@ import time
 sys.dont_write_bytecode = True
 from modules.header import *
 
-# Set Java options for memory management
-env = os.environ.copy()
-env["JAVA_OPTS"] = "-Xmx8g"
-
 parser = argparse.ArgumentParser(description="None")
 parser.add_argument("-I", "--input",required=True, type=str, help="None")
 args = parser.parse_args()
 
 INPUT_YAML = args.input
-SAMPLE_LIST = {}
-REFERENCE_LIST = {}
-stage = None
+
 # Load input YAML file
 with open(f"{INPUT_YAML}", "r") as f:
     input = yaml.safe_load(f)
@@ -38,15 +32,6 @@ if KNOWN_SITES:
     for site in KNOWN_SITES:
         known_sites_string += f"--known-sites {site} "
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    handlers=[
-        logging.FileHandler(f"{OUTDIR}/runtime.log", mode="a", encoding="utf-8"),
-        logging.StreamHandler(sys.stdout)
-    ]
-)
 # Classification and define sample information
 for sample in SAMPLES:
     ID = sample["id"]
@@ -84,42 +69,23 @@ for sample in SAMPLES:
         "final_vcf_file": f"{ID}.final.vcf",
         "xlsx_file" : f"{ID}.xlsx"
     }
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.FileHandler(f"{OUTDIR}/runtime.log", mode="a", encoding="utf-8"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
 logging.info(f"Reference information:\n{yaml.dump(REFERENCE_LIST, sort_keys=False, default_flow_style=False)}")
 logging.info(f"Sample information:\n{yaml.dump(SAMPLE_LIST, sort_keys=False, default_flow_style=False)}")
 # +----------------------------------------------------------------------------------------------------+ #
 # |                                        Mapping and Alignment                                       | #
 # +----------------------------------------------------------------------------------------------------+ #
 
-def mapping_and_alignment_stage():
-    for sample_id, info in SAMPLE_LIST.items():
-        start_time = time.time()
-        logging.info(f"Mapping and alignment sample: {sample_id}")
-        if info["read_length_type"] == "short":
-            mapping_and_alignment_BWA_mem(
-                SAMPLE_ID=sample_id,
-                PLATFORM=info["platform"],
-                FORWARD=info["read1"],
-                REVERSE=info["read2"],
-                REFERENCE=REFERENCE_LIST["genome"],
-                OUTDIR=OUTDIR,
-                SAMPLE_OUTDIR=info["sample_outdir"],
-                SAM_FILE=info["sam_file"]
-            )
-        else:
-            mapping_and_alignment_Minimap2(
-                SAMPLE_ID=sample_id,
-                PLATFORM=info["platform"],
-                FORWARD=info["read1"],
-                REVERSE=info["read2"],
-                REFERENCE=REFERENCE_LIST["genome"],
-                OUTDIR=OUTDIR,
-                SAMPLE_OUTDIR=info["sample_outdir"],
-                SAM_FILE=info["sam_file"]
-            )
-        end_time = time.time()
-        duration = (end_time - start_time) / 60  
-        logging.info(f"{sample_id} finished mapping and alignment in {duration:.2f} minutes")
-    logging.info("All samples finished mapping and alignment step.")
+
 # +----------------------------------------------------------------------------------------------------+ #
 # |                                      Post-mapping and Alignment                                    | #
 # +----------------------------------------------------------------------------------------------------+ #
