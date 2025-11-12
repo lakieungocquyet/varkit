8d0311b6a73d8337536fe7dfd7c1ceced070a879
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-12 09:56:02 +0700

    Update post_mapping_and_alignment_flow.py

--- src/modules/flow/post_mapping_and_alignment_flow.py
+++ src/modules/flow/post_mapping_and_alignment_flow.py
@@ -1,9 +1,11 @@
+import time
 from modules.header import *
 
-def post_mapping_and_alignment_flow(SAMPLE_LIST, KNOWN_SITES_STRING, OUTDIR):
+def post_mapping_and_alignment_flow(SAMPLE_LIST, REFERENCE_LIST, KNOWN_SITES_STRING, OUTDIR):
+    setup_logger(outdir = OUTDIR)
     for sample_id, info in SAMPLE_LIST.items():
         start_time = time.time()
-        logging.info(f"Post-mapping and alignment sample: {sample_id}")
+        logging_info(f"Post-mapping and alignment sample: {sample_id}")
         convert_and_sort(
             SAMPLE_OUTDIR=info["sample_outdir"],
             SAM_FILE=info["sam_file"],
@@ -14,23 +16,23 @@ def post_mapping_and_alignment_flow(SAMPLE_LIST, KNOWN_SITES_STRING, OUTDIR):
             SORTED_BAM_FILE=info["sorted_bam_file"],
             SAMPLE_OUTDIR=info["sample_outdir"],
             OUTDIR=OUTDIR,
-            MARKED_BAM_FILE=info["marked_bam_file"],
+            MARKED_BAM_FILE=info["sorted_marked_bam_file"],
         )
         baserecalibrator(
-            MARKED_BAM_FILE=info["marked_bam_file"],
+            MARKED_BAM_FILE=info["sorted_marked_bam_file"],
             SAMPLE_OUTDIR=info["sample_outdir"],
             KNOWN_SITES=KNOWN_SITES_STRING,
             REFERENCE=REFERENCE_LIST["genome"],
             OUTDIR=OUTDIR
         )
         applyBQSR(
-            MARKED_BAM_FILE=info["marked_bam_file"],
+            MARKED_BAM_FILE=info["sorted_marked_bam_file"],
             REFERENCE=REFERENCE_LIST["genome"],
             SAMPLE_OUTDIR=info["sample_outdir"],
             OUTDIR=OUTDIR,
-            RECAL_BAM_FILE=info["recal_bam_file"],
+            RECAL_BAM_FILE=info["sorted_marked_recal_bam_file"],
         )  
         end_time = time.time()
         duration = (end_time - start_time) / 60  
-        logging.info(f"{sample_id} finished post-mapping and alignment in {duration:.2f} minutes")
-logging.info("All samples finished post-mapping and alignment step.")    
+        logging_info(f"{sample_id} finished post-mapping and alignment in {duration:.2f} minutes")
+    logging_info("All samples finished post-mapping and alignment step.")    
