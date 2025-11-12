2e0255675104a7e69c3317c1600ae04e767740ad
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-12 09:56:13 +0700

    Update upstream_processing_flow.py

--- src/modules/flow/upstream_processing_flow.py
+++ src/modules/flow/upstream_processing_flow.py
@@ -2,12 +2,12 @@ from modules.header import *
 
 def upstream_processing_flow(INPUT_YAML):
     SAMPLES, GENOME, KNOWN_SITES, OUTDIR = initialize_from_yaml(INPUT_YAML)
-
+    setup_logger(outdir = OUTDIR)
     KNOWN_SITES_STRING = ""
 
     if KNOWN_SITES:
         for site in KNOWN_SITES:
-            known_sites_string += f"--known-sites {site} "
+            KNOWN_SITES_STRING += f"--known-sites {site} "
 
     SAMPLE_LIST, REFERENCE_LIST, GLOBAL_GVCF_LIST = init_samples(
         SAMPLES = SAMPLES, 
@@ -19,5 +19,6 @@ def upstream_processing_flow(INPUT_YAML):
     GVCF_FILE_STRING = ""
     for sample_id, info in SAMPLE_LIST.items():
         GVCF_FILE_STRING += f" -V {info['sample_outdir']}/{info['raw_gvcf_file']}"  
+    logging_info(f"Sample information:\n{yaml.dump(SAMPLE_LIST, sort_keys=False, default_flow_style=False)}")
 
     return OUTDIR, SAMPLE_LIST, REFERENCE_LIST, GLOBAL_GVCF_LIST, GVCF_FILE_STRING, KNOWN_SITES_STRING
