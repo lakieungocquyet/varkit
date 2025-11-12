7ee76aacfc85ca88a170d36947443f2eaea61845
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-12 09:56:19 +0700

    Update variant_annotation_flow.py

--- src/modules/flow/variant_annotation_flow.py
+++ src/modules/flow/variant_annotation_flow.py
@@ -1,6 +1,7 @@
 from modules.header import *
 
 def variant_annotation_flow(GLOBAL_GVCF_LIST, OUTDIR):
+    setup_logger(outdir = OUTDIR)
     snpEff_and_snpSift_annotation(
         NORMALIZED_GVCF_FILE = GLOBAL_GVCF_LIST["normalized_gvcf_file"], 
         OUTDIR = OUTDIR, 
