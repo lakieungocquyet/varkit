0714397c9bea6bdcdf9681a753ee6286bc079742
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-12 09:56:50 +0700

    Update default_pipeline.py

--- src/pipelines/default_pipeline.py
+++ src/pipelines/default_pipeline.py
@@ -1,27 +1,27 @@
 from modules.header import *
 
-def default_pipeline(INPUT_YAML):
-    # [STAGE 1]: UPSTREAM PROCESSING
-    OUTDIR, SAMPLE_LIST, REFERENCE_LIST, GLOBAL_GVCF_LIST, GVCF_FILE_STRING, KNOWN_SITES_STRING = upstream_processing_flow(INPUT_YAML = INPUT_YAML)
+def default_pipeline(OUTDIR, SAMPLE_LIST, REFERENCE_LIST, GLOBAL_GVCF_LIST, GVCF_FILE_STRING, KNOWN_SITES_STRING):
     setup_logger(outdir = OUTDIR)
-    # [STAGE 2]: MAPPING AND ALIGNMENT
+    # [STAGE 1]: MAPPING AND ALIGNMENT
     mapping_and_alignment_flow(
         SAMPLE_LIST = SAMPLE_LIST, 
         REFERENCE_LIST = REFERENCE_LIST,
         OUTDIR = OUTDIR
         )
-    # [STAGE 3]: POST MAPPING AND ALIGNMENT
+    # [STAGE 2]: POST MAPPING AND ALIGNMENT
     post_mapping_and_alignment_flow(
         SAMPLE_LIST = SAMPLE_LIST, 
+        REFERENCE= REFERENCE_LIST, 
         KNOWN_SITES_STRING = KNOWN_SITES_STRING, 
         OUTDIR = OUTDIR
         )
-    # [STAGE 4]: VARIANT CALLING
+    # [STAGE 3]: VARIANT CALLING
     variant_calling_flow(
         SAMPLE_LIST = SAMPLE_LIST, 
+        REFERENCE= REFERENCE_LIST, 
         OUTDIR = OUTDIR
         )
-    # [STAGE 5]: POST VARIANT CALLING
+    # [STAGE 4]: POST VARIANT CALLING
     post_variant_calling_flow(
         GVCF_FILE_LIST = GVCF_FILE_STRING,
         SAMPLE_LIST = SAMPLE_LIST, 
@@ -29,12 +29,12 @@ def default_pipeline(INPUT_YAML):
         OUTDIR = OUTDIR,
         GLOBAL_GVCF_LIST = GLOBAL_GVCF_LIST
         )
-    # [STAGE 6]: VARIANT ANNOTATION
+    # [STAGE 5]: VARIANT ANNOTATION
     variant_annotation_flow(
         GLOBAL_GVCF_LIST = GLOBAL_GVCF_LIST, 
         OUTDIR = OUTDIR, 
         )
-    # [STAGE 7]: DOWNSTREAM PROCESSING
+    # [STAGE 6]: DOWNSTREAM PROCESSING
     downstream_processing_flow(
         SAMPLE_LIST = SAMPLE_LIST, 
         GLOBAL_GVCF_LIST = GLOBAL_GVCF_LIST, 
