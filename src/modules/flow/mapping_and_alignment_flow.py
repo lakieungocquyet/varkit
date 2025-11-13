d82fad8c10ccd59ec85b44038e833076c954423a
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-13 14:46:47 +0700

    Update mapping_and_alignment_flow.py

--- src/modules/flow/mapping_and_alignment_flow.py
+++ src/modules/flow/mapping_and_alignment_flow.py
@@ -1,34 +1,47 @@
 import time
 from modules.header import *
 
-def mapping_and_alignment_flow(SAMPLE_LIST, REFERENCE_LIST, OUTDIR):
-    setup_logger(outdir = OUTDIR)
-    for sample_id, info in SAMPLE_LIST.items():
-        start_time = time.time()
-        logging_info(f"Mapping and alignment sample: {sample_id}")
+def mapping_and_alignment_flow(workflow_config):
+    # Extract data
+    OUTDIR = workflow_config["outdir"]
+    REFERENCE = workflow_config["reference_dict"]["genome"]
+    SAMPLE_INPUTS = workflow_config["sample_inputs"]
+    # setup_logger(outdir = OUTDIR)
+    # Function
+    for sample_id, info in SAMPLE_INPUTS.items():
+
+        SAMPLE_ID = sample_id
+        SAM_FILE = workflow_config["sample_outputs"][f"{sample_id}"]["sam_file"]
+        PLATFORM = workflow_config["sample_inputs"][f"{sample_id}"]["platform"]
+        READ_1 = workflow_config["sample_inputs"][f"{sample_id}"]["read_1"]
+        READ_2 = workflow_config["sample_inputs"][f"{sample_id}"]["read_2"]
+        SAMPLE_OUTDIR = workflow_config["sample_inputs"][f"{sample_id}"]["sample_outdir"]
+
+        # start_time = time.time()
+        # logging_info(f"Mapping and alignment sample: {sample_id}")
         if info["read_length_type"] == "short":
             mapping_and_alignment_BWA_mem(
-                SAMPLE_ID=sample_id,
-                PLATFORM=info["platform"],
-                FORWARD=info["read1"],
-                REVERSE=info["read2"],
-                REFERENCE=REFERENCE_LIST["genome"],
-                OUTDIR=OUTDIR,
-                SAMPLE_OUTDIR=info["sample_outdir"],
-                SAM_FILE=info["sam_file"]
+                sample_id=SAMPLE_ID,
+                platform=PLATFORM,
+                forward=READ_1,
+                reverse=READ_2,
+                reference=REFERENCE,
+                outdir=OUTDIR,
+                sample_outdir=SAMPLE_OUTDIR,
+                sam_file=SAM_FILE
             )
         else:
             mapping_and_alignment_Minimap2(
-                SAMPLE_ID=sample_id,
-                PLATFORM=info["platform"],
-                FORWARD=info["read1"],
-                REVERSE=info["read2"],
-                REFERENCE=REFERENCE_LIST["genome"],
-                OUTDIR=OUTDIR,
-                SAMPLE_OUTDIR=info["sample_outdir"],
-                SAM_FILE=info["sam_file"]
+                sample_id=SAMPLE_ID,
+                platform=PLATFORM,
+                forward=READ_1,
+                reverse=READ_2,
+                reference=REFERENCE,
+                outdir=OUTDIR,
+                sample_outdir=SAMPLE_OUTDIR,
+                sam_file=SAM_FILE
             )
-        end_time = time.time()
-        duration = (end_time - start_time) / 60  
-        logging_info(f"{sample_id} finished mapping and alignment in {duration:.2f} minutes")
-    logging_info("All samples finished mapping and alignment step.")
+    #     end_time = time.time()
+    #     duration = (end_time - start_time) / 60  
+    #     logging_info(f"{sample_id} finished mapping and alignment in {duration:.2f} minutes")
+    # logging_info("All samples finished mapping and alignment step.")
