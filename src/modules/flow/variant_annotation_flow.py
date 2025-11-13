203529b1770ba16296978b888b0255d4e57cea34
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-13 14:47:27 +0700

    Update variant_annotation_flow.py

--- src/modules/flow/variant_annotation_flow.py
+++ src/modules/flow/variant_annotation_flow.py
@@ -1,9 +1,14 @@
 from modules.header import *
 
-def variant_annotation_flow(GLOBAL_GVCF_LIST, OUTDIR):
-    setup_logger(outdir = OUTDIR)
+def variant_annotation_flow(workflow_config):
+
+    OUTDIR = workflow_config["outdir"]
+    COHORT_NORMALIZED_GVCF_FILE = workflow_config["cohort_outputs"]["cohort_normalized_gvcf_file"]
+    COHORT_SNPEFF_AND_SNPSIFT_ANNOTATED_GVCF_FILE = workflow_config["cohort_outputs"]["cohort_snpEff_and_snpSift_annotated_gvcf_file"]
+    
+    # setup_logger(outdir = OUTDIR)
     snpEff_and_snpSift_annotation(
-        NORMALIZED_GVCF_FILE = GLOBAL_GVCF_LIST["normalized_gvcf_file"], 
-        OUTDIR = OUTDIR, 
-        ANNOTATED_GVCF_FILE = GLOBAL_GVCF_LIST["snpEff_and_snpSift_annotated_gvcf_file"]
+        cohort_normalized_gvcf_file = COHORT_NORMALIZED_GVCF_FILE, 
+        outdir = OUTDIR, 
+        cohort_snpEff_and_snpSift_annotated_gvcf_file = COHORT_SNPEFF_AND_SNPSIFT_ANNOTATED_GVCF_FILE
         )
