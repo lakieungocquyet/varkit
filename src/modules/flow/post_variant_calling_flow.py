658abf8d913e8516bb7fb164266a4afcf0e7bc9e
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-13 14:47:04 +0700

    Update post_variant_calling_flow.py

--- src/modules/flow/post_variant_calling_flow.py
+++ src/modules/flow/post_variant_calling_flow.py
@@ -1,27 +1,35 @@
 from modules.header import *
 
-def post_variant_calling_flow(GVCF_FILE_STRING, REFERENCE_LIST, OUTDIR, GLOBAL_GVCF_LIST):
-    setup_logger(outdir = OUTDIR)
+def post_variant_calling_flow(workflow_config, gvcf_file_string):
+
+    OUTDIR = workflow_config["outdir"]
+    REFERENCE = workflow_config["reference_dict"]["genome"]
+    COMBINATION_GVCF_FILE = workflow_config["cohort_outputs"]["combination_gvcf_file"]
+    COHORT_GVCF_FILE = workflow_config["cohort_outputs"]["cohort_gvcf_file"]
+    COHORT_FILTERED_GVCF_FILE = workflow_config["cohort_outputs"]["cohort_filtered_gvcf_file"]
+    COHORT_NORMALIZED_GVCF_FILE = workflow_config["cohort_outputs"]["cohort_normalized_gvcf_file"]
+    
+    # setup_logger(outdir = OUTDIR)
     combine_gvcfs(
-        GVCF_FILE_STRING = GVCF_FILE_STRING,
-        REFERENCE = REFERENCE_LIST["genome"],
-        OUTDIR = OUTDIR,
-        COMBINED_GVCF_FILE = GLOBAL_GVCF_LIST["combined_gvcf_file"]
+        gvcf_file_string = gvcf_file_string,
+        reference = REFERENCE,
+        outdir = OUTDIR,
+        combination_gvcf_file = COMBINATION_GVCF_FILE
     )
     genotype_gvcfs(
-        COMBINED_GVCF_FILE = GLOBAL_GVCF_LIST["combined_gvcf_file"], 
-        REFERENCE = REFERENCE_LIST["genome"], 
-        OUTDIR = OUTDIR, 
-        COHORT_GVCF_FILE = GLOBAL_GVCF_LIST["cohort_gvcf_file"]
+        combination_gvcf_file = COMBINATION_GVCF_FILE, 
+        reference = REFERENCE, 
+        outdir = OUTDIR, 
+        cohort_gvcf_file = COHORT_GVCF_FILE
         )
     hard_filtration(
-        COHORT_GVCF_FILE = GLOBAL_GVCF_LIST["cohort_gvcf_file"], 
-        REFERENCE = REFERENCE_LIST["genome"],
-        OUTDIR = OUTDIR, 
-        FILTERED_GVCF_FILE = GLOBAL_GVCF_LIST["filtered_gvcf_file"]
+        cohort_gvcf_file = COHORT_GVCF_FILE, 
+        reference = REFERENCE,
+        outdir = OUTDIR, 
+        cohort_filtered_gvcf_file = COHORT_FILTERED_GVCF_FILE
     )
     variant_normalization(
-        FILTERED_GVCF_FILE = GLOBAL_GVCF_LIST["filtered_gvcf_file"],  
-        OUTDIR = OUTDIR, 
-        NORMALIZED_GVCF_FILE = GLOBAL_GVCF_LIST["normalized_gvcf_file"]
+        cohort_filtered_gvcf_file = COHORT_FILTERED_GVCF_FILE,  
+        outdir = OUTDIR, 
+        cohort_normalized_gvcf_file = COHORT_NORMALIZED_GVCF_FILE
         )
