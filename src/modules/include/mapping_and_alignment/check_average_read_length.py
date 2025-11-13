94f299891d2a4530fd2795f91553862d8773550a
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-13 14:48:11 +0700

    Delete check_average_read_length.py

--- src/modules/include/mapping_and_alignment/check_average_read_length.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def check_average_read_length(fastq):
-    command = f'seqtk seq -A "{fastq}" | awk \'{{if(NR%2==0){{sum+=length($0);n++}}}} END{{if(n>0) print sum/n; else print 0}}\''
-    average_length = subprocess.run(command, shell=True, capture_output=True, text=True)
-    return float(average_length.stdout.strip() or 0)
-
