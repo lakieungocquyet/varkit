ea90b3c608acd142b9001824878be98769d24c5f
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-13 16:52:48 +0700

    Update variant_filtration.py

--- src/modules/include/post_variant_calling/variant_filtration.py
+++ src/modules/include/post_variant_calling/variant_filtration.py
@@ -1,17 +1,17 @@
 import subprocess
 
-def hard_filtration(COMBINED_GVCF_FILE, REFERENCE, OUTDIR, FILTERED_GVCF_FILE):
+def hard_filtration(cohort_gvcf_file, reference, outdir, cohort_filtered_gvcf_file):
     command = f"""
-        /usr/bin/time -v -o {OUTDIR}/runtime.log \
+        /usr/bin/time -v -o {outdir}/runtime.log \
             gatk VariantFiltration \
-                -R {REFERENCE} \
-                -V {OUTDIR}/{COMBINED_GVCF_FILE} \
+                -R {reference} \
+                -V {outdir}/{cohort_gvcf_file} \
                 --filter-expression "vc.isSNP() && (QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0 || SOR > 3.0)" \
                 --filter-name "MG_SNP_Filter" \
                 --filter-expression "vc.isIndel() && (QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0)" \
                 --filter-name "MG_INDEL_Filter" \
-                -O {OUTDIR}/{FILTERED_GVCF_FILE} \
-        2>> {OUTDIR}/monitoring.log
+                -O {outdir}/{cohort_filtered_gvcf_file} \
+        2>> {outdir}/monitoring.log
     """
     subprocess.run(command, shell=True, check=True)
 
