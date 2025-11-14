6617667da8cd1b7dbd3f6e54773d6dce689ca7a5
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-14 10:51:44 +0700

    Update variant_calling_flow.py

--- src/modules/flow/variant_calling_flow.py
+++ src/modules/flow/variant_calling_flow.py
@@ -4,22 +4,21 @@ from modules.header import *
 def variant_calling_flow(workflow_config):
 
     OUTDIR = workflow_config["outdir"]
-    REFERENCE = workflow_config["reference_dict"]["genome"]
+    REFERENCE_GENOME = workflow_config["reference_dict"]["reference_genome"]
     SAMPLE_INPUTS = workflow_config["sample_inputs"]
     # setup_logger(outdir = OUTDIR)
     for sample_id, info in SAMPLE_INPUTS.items():
-
-        RECAL_BAM_FILE = workflow_config["sample_outputs"][f"{sample_id}"]["recal_bam_file"]
-        SAMPLE_OUTDIR = workflow_config["sample_inputs"][f"{sample_id}"]["sample_outdir"]
-        GVCF_FILE = workflow_config["sample_outputs"][f"{sample_id}"]["gvcf_file"]
+        SAMPLE_RECAL_BAM_FILE = workflow_config["sample_outputs"][f"{sample_id}"]["sample_recal_bam_file"]
+        SAMPLE_OUTDIR = workflow_config["sample_outputs"][f"{sample_id}"]["sample_outdir"]
+        SAMPLE_GVCF_FILE = workflow_config["sample_outputs"][f"{sample_id}"]["sample_gvcf_file"]
         # start_time = time.time()
         # logging_info(f"Variant calling sample: {sample_id}")
         genomic_SNPs_and_Indels_calling_GATK(
-            recal_bam_file=RECAL_BAM_FILE,
-            reference=REFERENCE,
+            sample_recal_bam_file=SAMPLE_RECAL_BAM_FILE,
+            reference_genome=REFERENCE_GENOME,
             sample_outdir=SAMPLE_OUTDIR,
             outdir=OUTDIR,
-            gvcf_file=GVCF_FILE,
+            sample_gvcf_file=SAMPLE_GVCF_FILE,
         )
         # end_time = time.time()
         # duration = (end_time - start_time) / 60  
