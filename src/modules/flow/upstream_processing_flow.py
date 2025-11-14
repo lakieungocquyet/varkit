a6bbe4986b6ff56a33174525929ef29b6c52cb61
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-14 10:51:34 +0700

    Update upstream_processing_flow.py

--- src/modules/flow/upstream_processing_flow.py
+++ src/modules/flow/upstream_processing_flow.py
@@ -3,11 +3,6 @@ from modules.header import *
 def upstream_processing_flow(input_yaml):
     SAMPLES, GENOME, KNOWN_SITES, OUTDIR = initialize_from_yaml(input_yaml)
     # setup_logger(outdir = OUTDIR)
-    KNOWN_SITES_STRING = ""
-
-    if KNOWN_SITES:
-        for site in KNOWN_SITES:
-            KNOWN_SITES_STRING += f"--known-sites {site} "
 
     WORKFLOW_CONFIG = init_samples(
         SAMPLES = SAMPLES, 
@@ -16,10 +11,16 @@ def upstream_processing_flow(input_yaml):
         OUTDIR = OUTDIR
         )
     
+    KNOWN_SITES_STRING = ""
+    if KNOWN_SITES:
+        for site in KNOWN_SITES:
+            KNOWN_SITES_STRING += f"--known-sites {site} "
+
     GVCF_FILE_STRING = ""
     for sample_id, info in WORKFLOW_CONFIG["sample_outputs"].items():
-        sample_outdir = WORKFLOW_CONFIG["sample_inputs"][sample_id]["sample_outdir"]
-        GVCF_FILE_STRING += f" -V {sample_outdir}/{info['gvcf_file']}"  
+        sample_outdir = WORKFLOW_CONFIG["sample_outputs"][sample_id]["sample_outdir"]
+        sample_gvcf_file = WORKFLOW_CONFIG["sample_outputs"][sample_id]["sample_gvcf_file"]
+        GVCF_FILE_STRING += f" -V {sample_outdir}/{sample_gvcf_file}"  
     
     # sample_info_str = yaml.dump(
     #     WORKFLOW_CONFIG["sample_inputs"], sort_keys=False, default_flow_style=False
