b88b021945e883868b5c9624a23cf43c2f1ef981
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-14 10:52:06 +0700

    Update mapping_and_alignment_BWA_mem.py

--- src/modules/include/mapping_and_alignment/mapping_and_alignment_BWA_mem.py
+++ src/modules/include/mapping_and_alignment/mapping_and_alignment_BWA_mem.py
@@ -1,15 +1,15 @@
 import subprocess
 
-def mapping_and_alignment_BWA_mem(forward, reverse, sample_id, platform, reference, sample_outdir, outdir, sam_file):
+def mapping_and_alignment_BWA_mem(forward, reverse, sample_id, platform, reference_genome, sample_outdir, outdir, sample_sam_file):
 
     command = f"""
         /usr/bin/time -v -a -o {outdir}/runtime.log \
             bwa mem -t 8 \
                 -R "@RG\\tID:{sample_id}\\tLB:lib1\\tPL:{platform}\\tPU:unit1\\tSM:{sample_id}" \
-                {reference} \
+                {reference_genome} \
                 {forward} \
                 {reverse} \
-            > {sample_outdir}/{sam_file} \
+            > {sample_outdir}/{sample_sam_file} \
         2>> {outdir}/monitoring.log
     """
     subprocess.run(command, shell=True, check=True)
