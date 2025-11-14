5ae2db7153fa10e7acdae887def07cab50c067b7
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-14 10:52:00 +0700

    Update variant_selection.py

--- src/modules/include/downstream_processing/variant_selection.py
+++ src/modules/include/downstream_processing/variant_selection.py
@@ -1,14 +1,14 @@
 import subprocess
 
-def select_variant_by_sample(cohort_snpEff_and_snpSift_annotated_gvcf_file, sample_id, reference, sample_outdir, outdir, vcf_file):
+def select_variant_by_sample(cohort_snpEff_and_snpSift_annotated_vcf_file, sample_id, reference_genome, sample_outdir, outdir, sample_vcf_file):
     command = f"""
         /usr/bin/time -v -a -o {outdir}/runtime.log \
             gatk SelectVariants \
-                -V {outdir}/{cohort_snpEff_and_snpSift_annotated_gvcf_file} \
-                -R {reference} \
+                -V {outdir}/{cohort_snpEff_and_snpSift_annotated_vcf_file} \
+                -R {reference_genome} \
                 --sample-name {sample_id} \
                 --exclude-non-variants \
-                -O {sample_outdir}/{vcf_file} \
+                -O {sample_outdir}/{sample_vcf_file} \
         2>> {outdir}/monitoring.log
         """
     subprocess.run(command, shell=True, check=True)
