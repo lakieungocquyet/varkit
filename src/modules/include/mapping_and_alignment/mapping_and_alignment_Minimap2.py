ea3efb6320e68dd0a612ea06d4ced3121d73a6c7
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-14 10:52:11 +0700

    Update mapping_and_alignment_Minimap2.py

--- src/modules/include/mapping_and_alignment/mapping_and_alignment_Minimap2.py
+++ src/modules/include/mapping_and_alignment/mapping_and_alignment_Minimap2.py
@@ -1,14 +1,14 @@
 import subprocess
 
-def mapping_and_alignment_Minimap2(forward, reverse, sample_id, platform, reference, sample_outdir, outdir, sam_file):
+def mapping_and_alignment_Minimap2(forward, reverse, sample_id, platform, reference_genome, sample_outdir, outdir, sample_sam_file):
     command = f"""
         /usr/bin/time -v -o {outdir}/runtime.log \
             minimap2 -x -a \
-                {reference}.mmi \
+                {reference_genome}.mmi \
                 -R "@RG\\tID:{sample_id}\\tLB:lib1\\tPL:{platform}\\tPU:unit1\\tSM:{sample_id}" \
                 {forward} \
                 {reverse} \
-                > {sample_outdir}/{sam_file} \
+                > {sample_outdir}/{sample_sam_file} \
         2>> {outdir}/monitoring.log
     """
     subprocess.run(command, shell=True, check=True)
