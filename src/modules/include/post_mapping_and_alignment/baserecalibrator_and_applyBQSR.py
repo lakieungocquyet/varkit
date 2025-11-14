901817e301218a148b6628f0fa83040b3f26510b
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-14 10:52:16 +0700

    Update baserecalibrator_and_applyBQSR.py

--- src/modules/include/post_mapping_and_alignment/baserecalibrator_and_applyBQSR.py
+++ src/modules/include/post_mapping_and_alignment/baserecalibrator_and_applyBQSR.py
@@ -1,25 +1,25 @@
 import subprocess
 
-def baserecalibrator(marked_bam_file, known_sites_string, reference, sample_outdir, outdir):
+def baserecalibrator(sample_marked_bam_file, known_sites_string, reference_genome, sample_outdir, outdir):
     command = f"""
     /usr/bin/time -v -a -o {outdir}/runtime.log \
         gatk BaseRecalibrator \
-            -I {sample_outdir}/{marked_bam_file} \
-            -R {reference} \
+            -I {sample_outdir}/{sample_marked_bam_file} \
+            -R {reference_genome} \
             {known_sites_string} \
             -O {sample_outdir}/recal_data.table \
     2>> {outdir}/monitoring.log
     """
     subprocess.run(command, shell=True, check=True)
 
-def applyBQSR (marked_bam_file, reference, sample_outdir, outdir, recal_bam_file):
+def applyBQSR (sample_marked_bam_file, reference_genome, sample_outdir, outdir, sample_recal_bam_file):
     command = f"""
         /usr/bin/time -v -a -o {outdir}/runtime.log \
             gatk ApplyBQSR \
-                -I {sample_outdir}/{marked_bam_file} \
-                -R {reference} \
+                -I {sample_outdir}/{sample_marked_bam_file} \
+                -R {reference_genome} \
                 --bqsr-recal-file {sample_outdir}/recal_data.table \
-                -O {sample_outdir}/{recal_bam_file} \
+                -O {sample_outdir}/{sample_recal_bam_file} \
         2>> {outdir}/monitoring.log
     """
     subprocess.run(command, shell=True, check=True)
