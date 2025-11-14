f09524ae37b8ac0c8d146aeee1356bca5906b454
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-14 10:52:32 +0700

    Update genomic_variant_combination.py

--- src/modules/include/post_variant_calling/genomic_variant_combination.py
+++ src/modules/include/post_variant_calling/genomic_variant_combination.py
@@ -1,12 +1,12 @@
 import subprocess
 
-def combine_gvcfs(gvcf_file_string, reference, outdir, combination_gvcf_file):
+def combine_gvcfs(gvcf_file_string, reference_genome, outdir, cohort_gvcf_file):
     command = f"""
         /usr/bin/time -v -a -o {outdir}/runtime.log \
             gatk CombineGVCFs \
-                -R {reference} \
+                -R {reference_genome} \
                 {gvcf_file_string} \
-                -O {outdir}/{combination_gvcf_file} \
+                -O {outdir}/{cohort_gvcf_file} \
         2>> {outdir}/monitoring.log
     """
     subprocess.run(command, shell=True, check=True)
