73661bb7e1e4cefae52db3a0617091a403270ce5
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-14 10:52:27 +0700

    Update markduplicates.py

--- src/modules/include/post_mapping_and_alignment/markduplicates.py
+++ src/modules/include/post_mapping_and_alignment/markduplicates.py
@@ -1,11 +1,11 @@
 import subprocess
 
-def markduplicates(sorted_bam_file, sample_outdir, outdir, marked_bam_file):
+def markduplicates(sample_sorted_bam_file, sample_outdir, outdir, sample_marked_bam_file):
     command = f"""
         /usr/bin/time -v -a -o {outdir}/runtime.log \
             gatk MarkDuplicates \
-                -I {sample_outdir}/{sorted_bam_file} \
-                -O {sample_outdir}/{marked_bam_file} \
+                -I {sample_outdir}/{sample_sorted_bam_file} \
+                -O {sample_outdir}/{sample_marked_bam_file} \
                 -M {sample_outdir}/output.metrics.txt \
         2>> {outdir}/monitoring.log
     """
