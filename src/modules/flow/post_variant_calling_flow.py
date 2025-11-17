31b69e2463bc265cc90e78beaec5f4a20ae5fb25
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-17 17:10:40 +0700

    Update post_variant_calling_flow.py

--- src/modules/flow/post_variant_calling_flow.py
+++ src/modules/flow/post_variant_calling_flow.py
@@ -2,34 +2,33 @@ from modules.header import *
 
 def post_variant_calling_flow(workflow_config, gvcf_file_string):
 
-    OUTDIR = workflow_config["outdir"]
+    TEMP_OUTDIR = workflow_config["temp_outdir"]
     REFERENCE_GENOME = workflow_config["reference_dict"]["reference_genome"]
     COHORT_GVCF_FILE = workflow_config["cohort_outputs"]["cohort_gvcf_file"]
     COHORT_VCF_FILE = workflow_config["cohort_outputs"]["cohort_vcf_file"]
     COHORT_FILTERED_VCF_FILE = workflow_config["cohort_outputs"]["cohort_filtered_vcf_file"]
     COHORT_NORMALIZED_VCF_FILE = workflow_config["cohort_outputs"]["cohort_normalized_vcf_file"]
     
-    # setup_logger(outdir = OUTDIR)
     combine_gvcfs(
         gvcf_file_string = gvcf_file_string,
         reference_genome=REFERENCE_GENOME,
-        outdir = OUTDIR,
-        cohort_gvcf_file = COHORT_GVCF_FILE
+        outdir = TEMP_OUTDIR,
+        output_file = COHORT_GVCF_FILE
     )
     genotype_gvcfs(
-        cohort_gvcf_file = COHORT_GVCF_FILE, 
+        input_file = COHORT_GVCF_FILE, 
         reference_genome=REFERENCE_GENOME,
-        outdir = OUTDIR, 
-        cohort_vcf_file = COHORT_VCF_FILE
+        outdir = TEMP_OUTDIR, 
+        output_file = COHORT_VCF_FILE
         )
     hard_filtration(
-        cohort_vcf_file = COHORT_VCF_FILE, 
+        input_file = COHORT_VCF_FILE, 
         reference_genome=REFERENCE_GENOME,
-        outdir = OUTDIR, 
-        cohort_filtered_vcf_file = COHORT_FILTERED_VCF_FILE
+        outdir = TEMP_OUTDIR, 
+        output_file = COHORT_FILTERED_VCF_FILE
     )
     variant_normalization(
-        cohort_filtered_vcf_file = COHORT_FILTERED_VCF_FILE,
-        outdir = OUTDIR, 
-        cohort_normalized_vcf_file = COHORT_NORMALIZED_VCF_FILE
+        input_file = COHORT_FILTERED_VCF_FILE,
+        outdir = TEMP_OUTDIR, 
+        output_file = COHORT_NORMALIZED_VCF_FILE
         )
