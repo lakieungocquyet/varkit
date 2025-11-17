56bc6c3d312443f832430ef54a5cfaca5769d725
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-17 15:36:52 +0700

    Update sanitization.py

--- src/modules/utils/sanitization.py
+++ src/modules/utils/sanitization.py
@@ -1,12 +1,12 @@
 import subprocess
 
-def sanitization(sample_vcf_file, sample_outdir, outdir, sample_final_vcf_file):
+def sanitization(input_file, sample_outdir, outdir, output_file):
     command = f"""
         /usr/bin/time -v -a -o {outdir}/runtime.log \
             grep -vE \
                 '##source=|##bcftools_normVersion=|##SnpEffVersion=|##SnpSiftVersion=|##SnpEffCmd=|##SnpSiftCmd=|##GATKCommandLine=|##bcftools_normCommand=' \
-                {sample_outdir}/{sample_vcf_file} \
-                > {sample_outdir}/{sample_final_vcf_file}
+                {sample_outdir}/{input_file} \
+                > {sample_outdir}/{output_file}
         2>> {outdir}/monitoring.log
         """
     subprocess.run(command, shell=True, check=True)
