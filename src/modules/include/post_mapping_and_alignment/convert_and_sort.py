90064893d2d00d0bdea3364b7dd34f16a5757c44
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-17 15:36:14 +0700

    Update convert_and_sort.py

--- src/modules/include/post_mapping_and_alignment/convert_and_sort.py
+++ src/modules/include/post_mapping_and_alignment/convert_and_sort.py
@@ -1,9 +1,9 @@
 import subprocess
-def convert_and_sort(sample_sam_file, sample_outdir, outdir, sample_sorted_bam_file):
+def convert_and_sort(input_file, sample_outdir, outdir, output_file):
     command = f"""
         /usr/bin/time -v -a -o {outdir}/runtime.log bash -c '\
-            samtools view -@ 8 -Sb {sample_outdir}/{sample_sam_file} | \
-            samtools sort -@ 8 -o {sample_outdir}/{sample_sorted_bam_file}' \
+            samtools view -@ 8 -Sb {sample_outdir}/{input_file} | \
+            samtools sort -@ 8 -o {sample_outdir}/{output_file}' \
         2>> {outdir}/monitoring.log
     """
     subprocess.run(command, shell=True, check=True)
