2776f56a7f435b32274c78a9c7d956f342bb7bc4
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-17 15:36:39 +0700

    Update variant_normalization.py

--- src/modules/include/post_variant_calling/variant_normalization.py
+++ src/modules/include/post_variant_calling/variant_normalization.py
@@ -1,12 +1,12 @@
 import subprocess
 
-def variant_normalization(cohort_filtered_vcf_file, outdir, cohort_normalized_vcf_file):
+def variant_normalization(input_file, outdir, output_file):
     command = f"""
         /usr/bin/time -v -a -o {outdir}/runtime.log \
             bcftools norm -Ov -m-any \
                 --multi-overlaps . \
-                {outdir}/{cohort_filtered_vcf_file} \
-                -o {outdir}/{cohort_normalized_vcf_file} \
+                {outdir}/{input_file} \
+                -o {outdir}/{output_file} \
         2>> {outdir}/monitoring.log
     """
     subprocess.run(command, shell=True, check=True)
