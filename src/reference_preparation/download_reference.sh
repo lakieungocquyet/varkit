bf974fc5ed89fb853cc28af2e33dece9114a2d0a
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-17 15:36:56 +0700

    Update download_reference.sh

--- src/reference_preparation/download_reference.sh
+++ src/reference_preparation/download_reference.sh
@@ -1,2 +1,15 @@
 #! /bin/bash
 
+
+wget -P /home/lknq/anotation https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/archive_2.0/2024/clinvar_20240716.vcf.gz
+
+wget -P /home/lknq/anotation https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/archive_2.0/2024/clinvar_20240716.vcf.gz.tbi
+
+wget -P /home/lknq/anotation https://snpeff.blob.core.windows.net/databases/dbs/GRCh37/dbNSFP_4.1a/dbNSFP4.1a.txt.gz
+
+wget -P /home/lknq/anotation https://snpeff.blob.core.windows.net/databases/dbs/GRCh37/dbNSFP_4.1a/dbNSFP4.1a.txt.gz.tbi
+
+wget -P /home/lknq/anotation https://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh37p13/VCF/All_20180423.vcf.gz 
+
+wget -P /home/lknq/anotation https://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh37p13/VCF/All_20180423.vcf.gz.tbi
+
