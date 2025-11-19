7c6bca89a6ed631bcdab3c51b1462eac784349d9
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-20 05:47:58 +0700

    Update downstream_processing_flow.py

--- src/modules/flow/downstream_processing_flow.py
+++ src/modules/flow/downstream_processing_flow.py
@@ -3,39 +3,34 @@ from modules.header import *
 
 def downstream_processing_flow(workflow_config):
 
-    OUTDIR = workflow_config["outdir"]
+    TEMP_OUTDIR = workflow_config["temp_outdir"]
     REFERENCE_GENOME = workflow_config["reference_dict"]["reference_genome"]
     SAMPLE_INPUTS = workflow_config["sample_inputs"]
-    COHORT_SNPEFF_AND_SNPSIFT_ANNOTATED_VCF_FILE = workflow_config["cohort_outputs"]["cohort_snpEff_and_snpSift_annotated_vcf_file"]
-    
+    COHORT_DBNSFP_ANNOTATED_VCF_FILE = workflow_config["cohort_outputs"]["cohort_dbnsfp_annotated_vcf_file"]
+    COHORT_FINAL_VCF_FILE = workflow_config["cohort_outputs"]["cohort_final_vcf_file"]
+  
     for sample_id, info in SAMPLE_INPUTS.items():
         start_time = time.time()
         log.info(f"Downstream processing sample: {sample_id}")
         SAMPLE_ID = sample_id
-        SAMPLE_OUTDIR = workflow_config["sample_outputs"][f"{sample_id}"]["sample_outdir"]
+        TEMP_SAMPLE_OUTDIR = workflow_config["sample_outputs"][f"{sample_id}"]["temp_sample_outdir"]
         SAMPLE_VCF_FILE = workflow_config["sample_outputs"][f"{sample_id}"]["sample_vcf_file"]
-        SAMPLE_FINAL_VCF_FILE = workflow_config["sample_outputs"][f"{sample_id}"]["sample_final_vcf_file"]
-        SAMPLE_XLSX_FILE = workflow_config["report_outputs"][f"{sample_id}"]["sample_xlsx_file"]
 
-        select_variant_by_sample(
-            cohort_snpEff_and_snpSift_annotated_vcf_file = COHORT_SNPEFF_AND_SNPSIFT_ANNOTATED_VCF_FILE, 
-            sample_id = SAMPLE_ID, 
-            reference_genome=REFERENCE_GENOME,
-            sample_outdir = SAMPLE_OUTDIR, 
-            outdir = OUTDIR, 
-            sample_vcf_file = SAMPLE_VCF_FILE
-            )
         sanitization(
-            sample_vcf_file = SAMPLE_VCF_FILE,
-            sample_outdir = SAMPLE_OUTDIR, 
-            outdir = OUTDIR, 
-            sample_final_vcf_file = SAMPLE_FINAL_VCF_FILE
+            input_file = COHORT_DBNSFP_ANNOTATED_VCF_FILE,
+            outdir = TEMP_OUTDIR, 
+            output_file = COHORT_FINAL_VCF_FILE
             )
-        export_to_XLSX(
-            sample_final_vcf_file = SAMPLE_FINAL_VCF_FILE,
-            sample_outdir = SAMPLE_OUTDIR, 
-            sample_xlsx_file = SAMPLE_XLSX_FILE
+        
+        select_variant_by_sample(
+            input_file = COHORT_FINAL_VCF_FILE, 
+            sample_id = SAMPLE_ID, 
+            reference_genome = REFERENCE_GENOME,
+            sample_outdir = TEMP_SAMPLE_OUTDIR, 
+            outdir = TEMP_OUTDIR, 
+            output_file = SAMPLE_VCF_FILE
             )
+
         end_time = time.time()
         duration = (end_time - start_time) / 60 
         log.info(f"{sample_id} finished downstream processing in {duration:.2f} minutes")
