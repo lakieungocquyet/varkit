f1495e1cd3329ebacbfa58a624c4bfc5717fffdb
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-20 05:49:50 +0700

    Create convert_and_sort_Samtools.py

---
+++ src/modules/include/post_mapping_and_alignment/convert_and_sort_Samtools.py
@@ -0,0 +1,9 @@
+import subprocess
+def convert_and_sort_Samtools(input_file, sample_outdir, outdir, output_file):
+    command = f"""
+        /usr/bin/time -v -a -o {outdir}/runtime.log bash -c '\
+            samtools view -@ 8 -Sb {sample_outdir}/{input_file} | \
+            samtools sort -@ 8 -o {sample_outdir}/{output_file}' \
+        2>> {outdir}/monitoring.log
+    """
+    subprocess.run(command, shell=True, check=True)
