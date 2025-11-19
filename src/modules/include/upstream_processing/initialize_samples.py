892b4ac421877ec32464830feb64384f4c3c20eb
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-20 05:51:19 +0700

    Create initialize_samples.py

---
+++ src/modules/include/upstream_processing/initialize_samples.py
@@ -0,0 +1,128 @@
+import yaml
+import os
+import subprocess
+import pathlib
+from datetime import datetime
+import uuid
+
+SAMPLE_INPUTS = {}
+REFERENCE_DICT = {}
+SAMPLE_OUTPUTS = {}
+COHORT_OUTPUTS = {}
+REPORT_OUTPUTS = {}
+
+WORKFLOW_CONFIG = {}
+
+def create_temp_outdir(root):
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    unique_id = uuid.uuid4().hex[:8]
+    path = pathlib.Path(root) / f"{timestamp}_{unique_id}"
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
+def check_average_read_length(fastq):
+    command = f'seqtk seq -A "{fastq}" | awk \'{{if(NR%2==0){{sum+=length($0);n++}}}} END{{if(n>0) print sum/n; else print 0}}\''
+    average_length = subprocess.run(command, shell=True, capture_output=True, text=True)
+    return float(average_length.stdout.strip() or 0)
+
+def initialize_from_yaml(input_yaml):
+
+    SAMPLES = None
+    GENOME = None
+    KNOWN_SITES = None
+    OUTDIR = None
+    with open(f"{input_yaml}", "r") as f:
+        input = yaml.safe_load(f)
+        SAMPLES = input["sample"]
+        GENOME = input["reference"]["genome"]
+        KNOWN_SITES = input["reference"]["known_site"]
+        OUTDIR = input["output"]["directory"]
+    return SAMPLES, GENOME, KNOWN_SITES, OUTDIR
+
+def initialize_samples(SAMPLES, GENOME, KNOWN_SITES, OUTDIR):
+
+    ID = None
+    R1 = None
+    R2 = None
+    FORWARD_AVERAGE_LENGTH = None
+    REVERSE_AVERAGE_LENGTH = None
+    AVERAGE_LENGTH = None
+    READ_TYPE = None
+
+    TEMP_OUTDIR = create_temp_outdir(
+        root = "/otp/varkit/jobspace"
+    )
+
+    for sample in SAMPLES:
+        ID = sample["id"]
+        R1 = sample["read1"]
+        R2 = sample["read2"]
+        FORWARD_AVERAGE_LENGTH = check_average_read_length(R1)
+        REVERSE_AVERAGE_LENGTH = check_average_read_length(R2)
+        AVERAGE_LENGTH = (FORWARD_AVERAGE_LENGTH + REVERSE_AVERAGE_LENGTH) / 2   
+        if AVERAGE_LENGTH >= 200:
+            READ_TYPE = "long"
+        else:
+            READ_TYPE = "short"
+        SAMPLE_OUTDIR = os.path.join(OUTDIR, ID)
+        TEMP_SAMPLE_OUTDIR = os.path.join(TEMP_OUTDIR, ID)
+        os.makedirs(SAMPLE_OUTDIR, exist_ok=True)
+        os.makedirs(TEMP_SAMPLE_OUTDIR, exist_ok=True)
+        SAMPLE_INPUTS[ID] = {
+            # INPUT INFO
+            "read_1": R1,
+            "read_2": R2,
+            "platform": "illumina",
+            "average_length": AVERAGE_LENGTH,
+            "read_length_type": READ_TYPE,
+            }
+        SAMPLE_OUTPUTS[ID] = {
+            # MAPPING/ALIGNMENT INFO
+            "sample_outdir": SAMPLE_OUTDIR, 
+            "temp_sample_outdir": TEMP_SAMPLE_OUTDIR,
+            "sample_sam_file": f"{ID}.sam",
+            "sample_sorted_bam_file": f"{ID}.sorted.bam",
+            "sample_marked_bam_file": f"{ID}.marked.bam",
+            "sample_recal_bam_file": f"{ID}.recal.bam",
+            "sample_final_bam_file": f"{ID}.final.bam",
+            # VARIANT INFO
+            "sample_gvcf_file": f"{ID}.g.vcf",
+            "sample_vcf_file": f"{ID}.vcf",
+            "sample_final_vcf_file": f"{ID}.final.vcf",      
+        }
+        REPORT_OUTPUTS[ID] = {
+            # REPORT INFO
+            "sample_xlsx_file" : f"{ID}.xlsx"
+        }
+
+    COHORT_OUTPUTS = {
+        "cohort_gvcf_file": "cohort.g.vcf",
+        "cohort_vcf_file": "cohort.vcf",
+        "cohort_filtered_vcf_file": f"cohort.filtered.vcf",
+        "cohort_normalized_vcf_file":f"cohort.normalized.vcf",
+        # ANNOTAION
+        "cohort_genome_annotated_vcf_file": f"cohort.genome_annotated.vcf",
+        "cohort_vartype_annotated_vcf_file": f"cohort.vartype_annotated.vcf",
+        "cohort_clinvar_annotated_vcf_file": f"cohort.clinvar_annotated.vcf",
+        "cohort_p3_1000g_annotated_vcf_file": f"cohort.p3_1000g_annotated.vcf",
+        "cohort_dbsnp_annotated_vcf_file": f"cohort.dbsnp_annotated.vcf",
+        "cohort_dbnsfp_annotated_vcf_file": f"cohort.dbnsfp_annotated.vcf",
+        "cohort_final_vcf_file": f"cohort.final.vcf",
+
+    }
+    REFERENCE_DICT = {
+        "reference_genome": GENOME,
+        "reference_known_sites": KNOWN_SITES
+    }
+
+    WORKFLOW_CONFIG = {
+        "sample_inputs": SAMPLE_INPUTS, 
+        "reference_dict": REFERENCE_DICT, 
+        "sample_outputs": SAMPLE_OUTPUTS, 
+        "cohort_outputs": COHORT_OUTPUTS, 
+        "report_outputs": REPORT_OUTPUTS,
+        "outdir": OUTDIR,
+        "temp_outdir": TEMP_OUTDIR
+        }
+
+    return WORKFLOW_CONFIG
