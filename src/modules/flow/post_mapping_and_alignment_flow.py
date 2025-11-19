82e163ec73cffa35018b327721bd2b145688a732
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-20 05:48:11 +0700

    Update post_mapping_and_alignment_flow.py

--- src/modules/flow/post_mapping_and_alignment_flow.py
+++ src/modules/flow/post_mapping_and_alignment_flow.py
@@ -7,6 +7,20 @@ def post_mapping_and_alignment_flow(workflow_config, known_sites_string):
     REFERENCE_GENOME = workflow_config["reference_dict"]["reference_genome"]
     SAMPLE_INPUTS = workflow_config["sample_inputs"]
 
+    samtools_ready = check_samtools_index(REFERENCE_GENOME)
+    if not samtools_ready:
+        log.error(f"Samtools index file missing for {REFERENCE_GENOME}, creating Samtools index.")
+        index_reference_genome_samtools(REFERENCE_GENOME)
+    else:
+        log.info(f"Samtools index file found for {REFERENCE_GENOME}, skipping indexing step.")
+    
+    gatk_ready = check_gatk_index(REFERENCE_GENOME)
+    if not gatk_ready:
+        log.error(f"GATK index file missing for {REFERENCE_GENOME}, creating GATK index.")
+        index_reference_genome_gatk(REFERENCE_GENOME)
+    else:
+        log.info(f"GATK index file found for {REFERENCE_GENOME}, skipping indexing step.")
+
     for sample_id, info in SAMPLE_INPUTS.items():
 
         SAMPLE_SAM_FILE = workflow_config["sample_outputs"][f"{sample_id}"]["sample_sam_file"]
@@ -17,26 +31,26 @@ def post_mapping_and_alignment_flow(workflow_config, known_sites_string):
 
         start_time = time.time()
         log.info(f"Post-mapping and alignment sample: {sample_id}")
-        convert_and_sort(
+        convert_and_sort_Samtools(
             sample_outdir=TEMP_SAMPLE_OUTDIR,
             input_file=SAMPLE_SAM_FILE,
             output_file=SAMPLE_SORTED_BAM_FILE,
             outdir=TEMP_OUTDIR
         )
-        markduplicates(
+        markduplicates_GATK(
             input_file=SAMPLE_SORTED_BAM_FILE,
             sample_outdir=TEMP_SAMPLE_OUTDIR,
             outdir=TEMP_OUTDIR,
             output_file=SAMPLE_MARKED_BAM_FILE,
         )
-        baserecalibrator(
+        recalibrate_bases_GATK(
             input_file=SAMPLE_MARKED_BAM_FILE,
             sample_outdir=TEMP_SAMPLE_OUTDIR,
             known_sites_string=known_sites_string,
             reference_genome=REFERENCE_GENOME,
             outdir=TEMP_OUTDIR
         )
-        applyBQSR(
+        apply_BQSR_GATK(
             input_file=SAMPLE_MARKED_BAM_FILE,
             reference_genome=REFERENCE_GENOME,
             sample_outdir=TEMP_SAMPLE_OUTDIR,
