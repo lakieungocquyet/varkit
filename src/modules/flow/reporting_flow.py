2d68ff916f2c59a39b13fb6ccfa078366558434e
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-20 17:37:45 +0700

    Update reporting_flow.py

--- src/modules/flow/reporting_flow.py
+++ src/modules/flow/reporting_flow.py
@@ -1,14 +1,14 @@
 import time
 from modules.header import *
 
-def downstream_processing_flow(workflow_config):
+def reporting_flow(workflow_config):
 
     SAMPLE_INPUTS = workflow_config["sample_inputs"]
     
     for sample_id, info in SAMPLE_INPUTS.items():
 
         start_time = time.time()
-        log.info(f"Downstream processing sample: {sample_id}")
+        log.info(f"Reporting sample: {sample_id}")
 
         TEMP_SAMPLE_OUTDIR = workflow_config["sample_outputs"][f"{sample_id}"]["temp_sample_outdir"]
         SAMPLE_VCF_FILE = workflow_config["sample_outputs"][f"{sample_id}"]["sample_vcf_file"]
@@ -19,9 +19,8 @@ def downstream_processing_flow(workflow_config):
             sample_outdir = TEMP_SAMPLE_OUTDIR, 
             output_file = SAMPLE_XLSX_FILE
             )
-        
         end_time = time.time()
         duration = (end_time - start_time) / 60 
-        log.info(f"{sample_id} finished downstream processing in {duration:.2f} minutes")
+        log.info(f"{sample_id} finished reporting in {duration:.2f} minutes")
 
-    log.info("All samples finished downstream processing step.")
+    log.info("All samples finished reporting step.")
