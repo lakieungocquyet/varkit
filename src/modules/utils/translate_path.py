9fa7ffe7beef96cb3827dfea89184b9372e77e40
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-22 00:01:08 +0700

    Update source tree

--- src/modules/utils/translate_path.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-def translate_path(path):
-    RESULT = subprocess.run(["wslpath", "-u", path], capture_output=True, text=True, check=True)
-    WSL_PATH = RESULT.stdout.strip()
-    return WSL_PATH
