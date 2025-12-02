e5cb8e6f35dfbb91a6c3e2fcea628800367a8968
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-12-02 10:20:52 +0700

    Update export_distro.sh

--- export_distro.sh
+++ export_distro.sh
@@ -1,7 +1,10 @@
-docker build -t varkit .
+#! /bin/bash
+set -e
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+docker build -t varkit "$SCRIPT_DIR"
+docker rm varkit
 docker run -t --name varkit varkit ls /
+mkdir -p /mnt/c/temp/
 docker export varkit > /mnt/c/temp/varkit.tar
 ## dockerContainerID=$(docker container ls -a | grep -i varkit | awk '{print $1}')
 ## docker export $dockerContainerID > /mnt/c/temp/varkit.tar
-
-
