2f93226a6a96b06089db3296bd7b6881d7302bc7
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-12-02 10:21:50 +0700

    Delete build_reference.sh

--- src/reference_preparation/build_reference.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-gatk LiftoverVcf \
-  -I /home/lknq/anotation/1000G.phase3.integrated.sites_only.no_MATCHED_REV.hg38.vcf \
-  -O /home/lknq/anotation/1000G.phase3.integrated.sites_only.no_MATCHED_REV.hg19.vcf \
-  -CHAIN /home/lknq/hg38ToHg19.over.chain \
-  -REJECT /home/lknq/anotation/rejected_variants.vcf \
-  -R /home/lknq/hg19/hg19.p13.plusMT.no_alt_analysis_set.fa
